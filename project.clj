96dbda766532cc9908adc2f62459cd6e2e2d25d3
David Frese <david.frese@active-group.de>
2017-01-29 23:57:41 +0100

    Change group id.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.dfrese/clojure-utils "0.1.0-SNAPSHOT"
+(defproject dfrese/clojure-utils "0.1.0-SNAPSHOT"
   :description "General purpose utils for Clojure and ClojureScript."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
